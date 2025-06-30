787ed9356420a5bece172f0c8e15be6cf069a253
Svvzi <johanartskill@gmail.com>
2025-06-30 09:13:55 +0000

    Initial commit

---
+++ src/main/java/com/johan/johan/JohanApplication.java
@@ -0,0 +1,13 @@
+package com.johan.johan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johan/johan/JohanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johan.johan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
